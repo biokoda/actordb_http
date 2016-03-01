97012ea63d7b24567c452a6fe0a08be1a417f94b
Denis Justinek <denis.justinek@biokoda.com>
2016-03-01 12:31:59 +0100

    Initial source commit. Boilerplate.

---
+++ src/actordb_http.erl
@@ -0,0 +1,12 @@
+% This Source Code Form is subject to the terms of the Mozilla Public
+% License, v. 2.0. If a copy of the MPL was not distributed with this
+% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+-module(actordb_http).
+
+-export([start/0, stop/0]).
+
+start() ->
+  ok.
+
+stop() ->
+  ok.
